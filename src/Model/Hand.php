28c3e2ae4d92453b365210ad2fc5cbf15243b215
robert611 <cyrkiel86@interia.pl>
2024-08-05 22:13:33 +0200

    Add function for recognizing full house

--- src/Model/Hand.php
+++ src/Model/Hand.php
@@ -186,4 +186,50 @@ enum Hand: int
 
         return false;
     }
+
+    /**
+     * @param Card[] $cards
+     */
+    public static function isRecognizedFullHouse(array $cards): bool
+    {
+        $ranks = [
+            CardRank::ACE->value => [],
+            CardRank::KING->value => [],
+            CardRank::QUEEN->value => [],
+            CardRank::JACK->value => [],
+            CardRank::TEN->value => [],
+            CardRank::NINE->value => [],
+            CardRank::EIGHT->value => [],
+            CardRank::SEVEN->value => [],
+            CardRank::SIX->value => [],
+            CardRank::FIVE->value => [],
+            CardRank::FOUR->value => [],
+            CardRank::THREE->value => [],
+            CardRank::TWO->value => [],
+        ];
+
+        foreach ($cards as $card) {
+            $ranks[$card->getRank()->value][] = $card;
+        }
+
+        $threeCards = false;
+        $pair = false;
+
+        /** @var Card[] $rankCards */
+        foreach ($ranks as $rankCards) {
+            if (count($rankCards) === 3) {
+                $threeCards = true;
+            }
+
+            if (count($rankCards) === 2) {
+                $pair = true;
+            }
+        }
+
+        if ($threeCards && $pair) {
+            return true;
+        }
+
+        return false;
+    }
 }
---
+++ tests/Unit/Model/HandRecognition/FullHouseRecognitionTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Tests\Unit\Model\HandRecognition;
+
+use App\Model\Card;
+use App\Model\CardRank;
+use App\Model\CardSuit;
+use App\Model\Hand;
+use PHPUnit\Framework\TestCase;
+
+class FullHouseRecognitionTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider fullHouseProvider
+     */
+    public function canRecognizeFullHouse(array $cards, bool $expectedResult): void
+    {
+        $result = Hand::isRecognizedFullHouse($cards);
+
+        self::assertSame($expectedResult, $result);
+    }
+
+    public function fullHouseProvider(): array
+    {
+        return [
+            [
+                [
+                    Card::create(CardRank::FIVE, CardSuit::HEARTS),
+                    Card::create(CardRank::JACK, CardSuit::DIAMONDS),
+                    Card::create(CardRank::JACK, CardSuit::SPADES),
+                    Card::create(CardRank::QUEEN, CardSuit::SPADES),
+                    Card::create(CardRank::QUEEN, CardSuit::CLUBS),
+                    Card::create(CardRank::QUEEN, CardSuit::HEARTS),
+                    Card::create(CardRank::KING, CardSuit::SPADES),
+                ],
+                true,
+            ],
+            [
+                [
+                    Card::create(CardRank::FIVE, CardSuit::HEARTS),
+                    Card::create(CardRank::QUEEN, CardSuit::DIAMONDS),
+                    Card::create(CardRank::JACK, CardSuit::DIAMONDS),
+                    Card::create(CardRank::TWO, CardSuit::SPADES),
+                    Card::create(CardRank::QUEEN, CardSuit::CLUBS),
+                    Card::create(CardRank::QUEEN, CardSuit::HEARTS),
+                    Card::create(CardRank::KING, CardSuit::SPADES),
+                ],
+                false,
+            ],
+            [
+                [
+                    Card::create(CardRank::FIVE, CardSuit::HEARTS),
+                    Card::create(CardRank::ACE, CardSuit::DIAMONDS),
+                    Card::create(CardRank::JACK, CardSuit::DIAMONDS),
+                    Card::create(CardRank::TWO, CardSuit::SPADES),
+                    Card::create(CardRank::QUEEN, CardSuit::CLUBS),
+                    Card::create(CardRank::FOUR, CardSuit::HEARTS),
+                    Card::create(CardRank::KING, CardSuit::SPADES),
+                ],
+                false,
+            ],
+            [
+                [
+                    Card::create(CardRank::FOUR, CardSuit::HEARTS),
+                    Card::create(CardRank::ACE, CardSuit::DIAMONDS),
+                    Card::create(CardRank::JACK, CardSuit::DIAMONDS),
+                    Card::create(CardRank::ACE, CardSuit::SPADES),
+                    Card::create(CardRank::FOUR, CardSuit::CLUBS),
+                    Card::create(CardRank::FOUR, CardSuit::DIAMONDS),
+                    Card::create(CardRank::JACK, CardSuit::SPADES),
+                ],
+                true,
+            ],
+        ];
+    }
+}

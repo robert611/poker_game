b802e7941b17fb526fa279021d35ee0630ebba66
robert611 <cyrkiel86@interia.pl>
2024-08-07 22:24:41 +0200

    Add function for recognizing one pair

--- src/Model/Hand.php
+++ src/Model/Hand.php
@@ -384,4 +384,39 @@ enum Hand: int
 
         return false;
     }
+
+    /**
+     * @param Card[] $cards
+     */
+    public static function isRecognizedOnePair(array $cards): bool
+    {
+        $ranks = [
+            CardRank::ACE->value => [],
+            CardRank::KING->value => [],
+            CardRank::QUEEN->value => [],
+            CardRank::JACK->value => [],
+            CardRank::TEN->value => [],
+            CardRank::NINE->value => [],
+            CardRank::EIGHT->value => [],
+            CardRank::SEVEN->value => [],
+            CardRank::SIX->value => [],
+            CardRank::FIVE->value => [],
+            CardRank::FOUR->value => [],
+            CardRank::THREE->value => [],
+            CardRank::TWO->value => [],
+        ];
+
+        foreach ($cards as $card) {
+            $ranks[$card->getRank()->value][] = $card;
+        }
+
+        /** @var Card[] $rankCards */
+        foreach ($ranks as $rankCards) {
+            if (count($rankCards) === 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
